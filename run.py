7762ed838b14a6a1b08f96a79bef3fd3e5b07af9
mdumbrava <mihaela.dmbrv@gmail.com>
2021-04-27 20:30:51 -0400

    modified run file

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ app = create_app()
 
 
 if __name__ == '__main__': #never change this "always main"
-    app.run(host='127.0.0.1', port=80, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
