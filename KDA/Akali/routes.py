7a1e8ce4837212a6a9d2dcb34d23b3d348e9698a
mdumbrava <mihaela.dmbrv@gmail.com>
2021-05-23 18:25:06 -0400

    akali fix

--- KDA/Akali/routes.py
+++ KDA/Akali/routes.py
@@ -6,4 +6,4 @@ from KDA.user import getuser
 def main():
    user=getuser()
    value = "Welcome to Akali's Page"
-   return render_template('AKali/Akali.html', value=value, user=user)
+   return render_template('Akali/Akali.html', value=value, user=user)
